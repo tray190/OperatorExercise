1d3db174c5d975f224124bba84970a6c4ab4cd53
tray190 <belltracy411@gmail.com>
2021-12-08 01:12:13 -0600

    OperatorExercise

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("enter the radius");
+            var radius = double.Parse(Console.ReadLine());
+            AreaOfCircle(225.32);
+
+
+            {
+
+            }
+                
+            public static double AreaOfCircle(double r)
+
+
+            {
+                return Math.PI * Math.Pow(ref, 2);
+               
+
+            }
+
+            
+
+        }
+           
+
+    }
+}
---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b == 4);
+            Console.WriteLine($"17/4 is 4 remainder 1");
+
+        }
+    }
+}
